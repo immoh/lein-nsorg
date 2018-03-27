321098badb5c51d3e15ee0ed8b6d1f64fca7b7e5
Immo Heikkinen <immo.heikkinen@zalando.fi>
2018-03-27 09:58:15 +0300

    Remove trailing slash from project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-nsorg "0.1.0-SNAPSHOT"
   :description "Leiningen plugin for organizing ns form"
-  :url "https://github.com/immoh/lein-nsorg/"
+  :url "https://github.com/immoh/lein-nsorg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clojure-term-colors "0.1.0"]
