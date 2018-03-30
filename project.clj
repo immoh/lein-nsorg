743352bcdc8d2c654f09f693e406016b4e61b68e
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-03-30 14:30:19 +0300

    Bump nsorg version to 0.1.1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clojure-term-colors "0.1.0"]
-                 [nsorg "0.1.0"]
+                 [nsorg "0.1.1"]
                  [org.clojure/tools.cli "0.3.5"]]
   :profiles {:dev {:dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}}
