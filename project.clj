8ea5cf0e3f26b7609eea8b156282c6bb8aac9616
Immo Heikkinen <immo.heikkinen@gmail.com>
2018-04-14 19:38:20 +0300

    Bump tools.cli version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clojure-term-colors "0.1.0"]
                  [nsorg "0.1.2"]
-                 [org.clojure/tools.cli "0.3.5"]]
+                 [org.clojure/tools.cli "0.3.6"]]
   :profiles {:dev {:dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}}
   :eval-in-leiningen true)
