850f343237dd56b26de8b26a6efbfe438e557e68
Immo Heikkinen <immo.heikkinen@gmail.com>
2019-04-22 19:56:52 +0300

    Move functionality to nsorg-cli

--- project.clj
+++ project.clj
@@ -3,9 +3,5 @@
   :url "https://github.com/immoh/lein-nsorg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clojure-term-colors "0.1.0"]
-                 [nsorg "0.2.0"]
-                 [org.clojure/tools.cli "0.4.1"]]
-  :profiles {:dev {:dependencies [[midje "1.9.6"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
+  :dependencies [[nsorg-cli "0.3.0"]]
   :eval-in-leiningen true)
--- src/leiningen/nsorg.clj
+++ src/leiningen/nsorg.clj
@@ -1,100 +1,8 @@
 (ns leiningen.nsorg
   "Organize ns forms in source files."
-  (:require [clojure.java.io :as io]
-            [clojure.stacktrace]
-            [clojure.string]
-            [clojure.tools.cli :as cli]
-            [leiningen.core.main :as lein]
-            [leiningen.nsorg.diff :as diff]
-            [nsorg.core :as nsorg])
-  (:import (java.io File)
-           (java.nio.file Paths)))
-
-(defn clojure-file? [^File file]
-  (and (.isFile file)
-       (re-matches #".+\.clj.?" (.getName file))))
-
-(defn excluded-file? [excluded-paths ^File file]
-  (if-not ((set excluded-paths) file)
-    file))
-
-(defn paths->file-seq [paths]
-  (->> paths
-       (map io/file)
-       (mapcat file-seq)))
-
-(defn find-clojure-files [paths excluded-paths]
-  (->> paths
-       (paths->file-seq)
-       (filter clojure-file?)
-       (filter (partial excluded-file? (paths->file-seq excluded-paths)))
-       (sort-by (memfn getAbsolutePath))))
-
-(defn prompt! [msg]
-  (locking *out*
-    (loop []
-      (print (str msg " [y/N] "))
-      (flush)
-      (case (clojure.string/lower-case (read-line))
-        ("y" "yes") true
-        ("" "n" "no") false
-        (recur)))))
-
-(defn ->absolute-path [s]
-  (.toAbsolutePath (Paths/get s (into-array String []))))
-
-(defn relativize-path [path]
-  (str (.relativize (->absolute-path "") (->absolute-path path))))
-
-(defn summarize [{:keys [replace interactive]} {:keys [files errors problems replaces]}]
-  (clojure.string/join ", " (keep identity
-                                  [(format "Checked %s files" files)
-                                   (when (and (zero? errors) (zero? problems))
-                                     "all good!")
-                                   (when (pos? errors)
-                                     (format "failed to check %s files" errors))
-                                   (when (and (pos? problems)
-                                              (or (not replace) interactive))
-                                     (format "found problems in %s files" problems))
-                                   (when (pos? replaces)
-                                     (format "fixed %s files" replaces))])))
-
-(defn organize-ns-form! [file replace? interactive?]
-  (let [path (relativize-path (.getAbsolutePath file))]
-    (try
-      (let [original-source (slurp file)
-            modified-source (nsorg/rewrite-ns-form original-source)
-            diff-chunks (diff/diff-chunks original-source modified-source)
-            problem? (seq diff-chunks)]
-        (when problem?
-          (lein/info (format "in %s:" path))
-          (lein/info (diff/format-diff diff-chunks))
-          (lein/info))
-        (let [replaced? (when (and problem?
-                                   replace?
-                                   (or (not interactive?)
-                                       (prompt! "Replace?")))
-                          (spit file modified-source)
-                          true)]
-          {:files    1
-           :problems (if problem? 1 0)
-           :replaces (if replaced? 1 0)}))
-      (catch Throwable t
-        (lein/warn (format "Failed to check path %s:" path))
-        (lein/warn (with-out-str (clojure.stacktrace/print-stack-trace t)))
-        {:errors 1}))))
-
-(defn organize-ns-forms! [paths excluded-paths options]
-  (reduce
-    (fn [result file]
-      (merge-with + result (organize-ns-form! file (:replace options) (:interactive options))))
-    {:errors 0 :files 0 :problems 0 :replaces 0}
-    (find-clojure-files paths excluded-paths)))
-
-(defn get-paths [arguments project]
-  (or (seq arguments)
-      (seq (mapcat #(get project %) [:source-paths :test-paths]))
-      ["./"]))
+  (:require [leiningen.core.main :as lein]
+            [nsorg.cli :as cli]
+            [nsorg.cli.terminal :as terminal]))
 
 (defn ^:no-project-needed nsorg
   "Leiningen plugin for organizing ns forms in source files.
@@ -110,26 +18,10 @@ Options:
   -i, --interactive   Ask before applying suggestion (requires --replace).
   -x, --exclude PATH  Path to exclude from analysis."
   [project & args]
-  (let [{:keys [options arguments]} (cli/parse-opts args [["-e" "--replace"]
-                                                          ["-i" "--interactive"]
-                                                          ["-x" "--exclude PATH"
-                                                           :default []
-                                                           :assoc-fn #(update %1 %2 conj %3)]])
-        paths (map relativize-path (get-paths arguments project))
-        excluded-paths (map relativize-path (:exclude options))]
-    (lein/info "Checking following paths:")
-    (doseq [path (sort paths)]
-      (lein/info path))
-    (lein/info)
-    (when (seq excluded-paths)
-      (lein/info "Ignoring following paths:")
-      (doseq [ignored-path (sort excluded-paths)]
-        (lein/info ignored-path))
-      (lein/info))
-    (let [result (organize-ns-forms! paths excluded-paths options)
-          summary (summarize options result)]
-      (if (or (pos? (:errors result))
-              (and (pos? (:problems result))
-                   (not (:replace options))))
-        (lein/abort summary)
-        (lein/info summary)))))
+  (binding [terminal/*info-fn* lein/info
+            terminal/*error-fn* lein/warn]
+    (let [{:keys [success? summary]} (cli/check args {:default-paths (mapcat #(get project %)
+                                                                             [:source-paths :test-paths])})]
+      (if success?
+        (lein/info summary)
+        (lein/abort summary)))))
